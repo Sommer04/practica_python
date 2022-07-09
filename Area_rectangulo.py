ee9d01b1f8988e4bdb0fc7d988382c21a5894dc9
sommerling <so.mauricio04@gmail.com>
2022-07-09 15:16:59 -0400

    Practica 1 elevar al cuadrado

---
+++ Area_rectangulo.py
@@ -0,0 +1,4 @@
+#Formula para calcular el area del rectangulo: base * altura
+base = float(input("Ingrese Base del rectangulo: "))
+altura = float(input("Ingrese Altura del rectangulo: "))
+print("El  Area del Rectangulo es: ", base*altura,"m^2")

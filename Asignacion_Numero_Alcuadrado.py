6042aeeb31898bd954ee82db2c51a22669f1b193
sommerling <so.mauricio04@gmail.com>
2022-07-09 15:07:44 -0400

    Practica 1 elevar al cuadrado

---
+++ Asignacion_Numero_Alcuadrado.py
@@ -0,0 +1,6 @@
+# ejercicio 1 Escribir un programa que lea un número por teclado y muestre en pantalla, el número elevado
+# al cuadrado
+#para realizar la potenciacion se utiliza **
+
+number = int(input("Ingrese numero a elevar al cuadrado: "))
+print("El resultado del numero elevado al cuadrado es: ", number**2)

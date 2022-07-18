bb0711831f4e65d3109d7cc7bea86a610c7edeb0
sommerling <so.mauricio04@gmail.com>
2022-07-18 19:43:23 -0400

    practica 2 imprimir letra vocar

--- Letra_vocal_NoVocal.py
+++ Letra_vocal_NoVocal.py
@@ -1,10 +1,10 @@
 #4 - Escribir un programa que pida al usuario una letra, y si es vocal
 # muestre el mensaje "Es vocal", de lo contrario que muestre "No es vocal"
-letra_vocal = str(input("Digite la Vocal: "))
+letra_vocal = input("Digite la Vocal: ")
 
-if letra_vocal == "a" or "e" or "i" or "o" or "u":
+if letra_vocal == "a" or  letra_vocal == "e" or letra_vocal == "i" or letra_vocal == "o" or letra_vocal == "u" :
     print("La vocal es: ", letra_vocal)
-
+elif letra_vocal == "A" or  letra_vocal == "E" or letra_vocal == "I" or letra_vocal == "O" or letra_vocal == "U" :
+    print("La vocal es: ", letra_vocal)
 else:
     print("La letra introducida no es una vocal")
-

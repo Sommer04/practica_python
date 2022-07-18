461b553814af619d406308eb1315691262e2b704
sommerling <so.mauricio04@gmail.com>
2022-07-18 16:48:53 -0400

    practica 2 valor absoluto del numero

---
+++ Valor_absoluto.py
@@ -0,0 +1,13 @@
+#3 - Escribir un programa que pida al usuario un número por teclado
+#y muestre el valor absoluto de dicho número.
+
+valor_absoluto = float (input("Ingrese el numero: "))
+
+if valor_absoluto >= 0:
+    print("su valor absoluto es = ",valor_absoluto)
+
+else :
+    valor_absoluto < 0
+    print("Su valor absoluto es = ", valor_absoluto*-1)
+
+
